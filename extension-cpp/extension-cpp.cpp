ce6124f8db57944dbfe45a3bed64a766a475a767
Jakub Mitura <53857487+jakubMitura14@users.noreply.github.com>
2022-03-12 20:56:30 +0100

    Add project files.

---
+++ extension-cpp/extension-cpp.cpp
@@ -0,0 +1,12 @@
+﻿// extension-cpp.cpp : Defines the entry point for the application.
+//
+
+#include "extension-cpp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
