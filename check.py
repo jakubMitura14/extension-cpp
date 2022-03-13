daeab729300bb0bbf2d4758e082d31869ac57e27
Jakub Mitura <53857487+jakubMitura14@users.noreply.github.com>
2022-03-13 11:15:37 +0100

    still compiles

--- check.py
+++ check.py
@@ -9,45 +9,8 @@ import lltm_baseline
 import lltm
 
 
-def check_equal(first, second, verbose):
-    if verbose:
-        print()
-    for i, (x, y) in enumerate(zip(first, second)):
-        x = x.cpu().detach().numpy()
-        y = y.cpu().detach().numpy()
-        if verbose:
-            print("x = {}".format(x.flatten()))
-            print("y = {}".format(y.flatten()))
-            print('-' * 80)
-        np.testing.assert_allclose(x, y, err_msg="Index: {}".format(i))
 
-
-def zero_grad(variables):
-    for variable in variables:
-        variable.grad.zero_()
-
-
-def get_grads(variables):
-    return [var.grad.clone() for var in variables]
-
-
-def check_forward(variables, with_cuda, verbose):
-    baseline_values = lltm_baseline.LLTMFunction.apply(*variables)
-    cpp_values = lltm.LLTMFunction.apply(*variables)
-
-    print('Forward: Baseline (Python) vs. C++ ... ', end='')
-    check_equal(baseline_values, cpp_values, verbose)
-    print('Ok')
-
-    if with_cuda:
-        cuda_values = lltm.LLTMFunction.apply(*variables)
-        print('Forward: Baseline (Python) vs. CUDA ... ', end='')
-        check_equal(baseline_values, cuda_values, verbose)
-        print('Ok')
-
-
-
-import lltm
+import lltm_cuda
 device = torch.device("cuda")
 
 kwargs = {'dtype': torch.float64,
@@ -63,5 +26,8 @@ b = torch.randn(1, 3 * 5, **kwargs)
 
 variables = [X, W, b, h, C]
 
-check_forward(variables, True, True)
+lltm_cuda.forward(X, W, b, h, C)
 
+#check_forward(variables, True, True)
+
+print("x = {}".format(W.flatten()))
--- lltm.py
+++ lltm.py
@@ -3,8 +3,8 @@ from torch import nn
 from torch.autograd import Function
 import torch
 
-import lltm_cuda
-
+#import lltm_cuda
+#lltm_cuda.forward()
 torch.manual_seed(42)
 
 
