89eec7bff859f255b49c43a377ff0489940e7227
Jakub Mitura <53857487+jakubMitura14@users.noreply.github.com>
2022-03-13 15:59:02 +0100

    still works

--- lltm_cuda_kernel.cu
+++ lltm_cuda_kernel.cu
@@ -748,9 +748,372 @@ __global__ void boolPrepareKernel(ForBoolKernelArgs<TYO> fbArgs
 
 
 
+/*
+    a) we define offsets in the result list to have the results organized and avoid overwiting
+    b) if metadata block is active we add it in the work queue
+*/
+
+
+/*
+we add here to appropriate queue data  about metadata of blocks of intrest
+minMaxesPos- marks in minmaxes the postion of global offset counter -12) global FP offset 13) global FnOffset
+offsetMetadataArr- arrays from metadata holding data about result list offsets it can be either fbArgs.metaData.fpOffset or fbArgs.metaData.fnOffset
+*/
+
+
+#pragma once
+__device__ inline void addToQueue(uint32_t linIdexMeta, uint8_t isGold
+    , unsigned int fpFnLocCounter[1], uint32_t localWorkQueue[1600], uint32_t localOffsetQueue[1600], unsigned int localWorkQueueCounter[1]
+    , uint8_t countIndexNumb, uint8_t isActiveIndexNumb, uint8_t offsetIndexNumb
+    , uint32_t* metaDataArr, MetaDataGPU metaData, unsigned int* minMaxes, uint32_t* workQueue) {
+
+    unsigned int count = metaDataArr[linIdexMeta * metaData.metaDataSectionLength + countIndexNumb];
+    //given fp is non zero we need to  add this to local queue
+    if (metaDataArr[linIdexMeta * metaData.metaDataSectionLength + isActiveIndexNumb] == 1) {
+
+        // printf("adding to local in first meta pass linIdexMeta %d isGold %d isActiveIndexNumb %d \n  ", linIdexMeta, isGold, isActiveIndexNumb);
+
+        count = atomicAdd_block(&fpFnLocCounter[0], count);
+        unsigned int  old = atomicAdd_block(&localWorkQueueCounter[0], 1);
+        //we check weather we still have space in shared memory
+        if (old < 1590) {// so we still have space in shared memory
+            // will be equal or above isGoldOffset  if it is gold pass
+            localWorkQueue[old] = linIdexMeta + (isGoldOffset * isGold);
+            localOffsetQueue[old] = uint32_t(count);
+        }
+        else {// so we do not have any space more in the sared memory  - it is unlikely so we will just in this case save immidiately to global memory
+            old = atomicAdd(&(minMaxes[9]), old);
+            //workQueue
+            workQueue[old] = linIdexMeta + (isGoldOffset * isGold);
+            //and offset 
+            metaDataArr[linIdexMeta * metaData.metaDataSectionLength + offsetIndexNumb] = atomicAdd(&(minMaxes[12]), count);
+        };
+    }
+}
+
+
+#pragma once
+template <typename PYO>
+__global__ void firstMetaPrepareKernel(ForBoolKernelArgs<PYO> fbArgs
+    , MetaDataGPU metaData, unsigned int* minMaxes, uint32_t* workQueue
+    , uint32_t* origArrs, uint32_t* metaDataArr) {
+
+    //////initializations
+    thread_block cta = this_thread_block();
+    char* tensorslice;// needed for iterations over 3d arrays
+   //local offset counters  for fp and fn's
+    __shared__ unsigned int fpFnLocCounter[1];
+    // used to store the start position in global memory for whole block
+    __shared__ unsigned int globalOffsetForBlock[1];
+    __shared__ unsigned int globalWorkQueueCounter[1];
+    //used as local work queue counter
+    __shared__ unsigned int localWorkQueueCounter[1];
+    //according to https://forums.developer.nvidia.com/t/find-the-limit-of-shared-memory-that-can-be-used-per-block/48556 it is good to keep shared memory below 16kb kilo bytes so it will give us 1600 length of shared memory
+    //so here we will store locally the calculated offsets and coordinates of meta data block of intrest marking also wheather we are  talking about gold or segmentation pass (fp or fn )
+    __shared__ uint32_t localWorkQueue[1600];
+    __shared__ uint32_t localOffsetQueue[1600];
+    if ((threadIdx.x == 0)) {
+        fpFnLocCounter[0] = 0;
+    }
+    if ((threadIdx.x == 1)) {
+        localWorkQueueCounter[0] = 0;
+    }
+    if ((threadIdx.x == 2)) {
+        globalWorkQueueCounter[0] = 0;
+    }
+    if ((threadIdx.x == 3)) {
+        globalOffsetForBlock[0] = 0;
+    }
+    sync(cta);
+
+
+    // classical grid stride loop - in case of unlikely event we will run out of space we will empty it prematurly
+    //main metadata iteration
+    for (uint32_t linIdexMeta = blockIdx.x * blockDim.x + threadIdx.x; linIdexMeta < metaData.totalMetaLength; linIdexMeta += blockDim.x * gridDim.x) {
+
+        // if ((threadIdx.x == 0) && (threadIdx.y == 0)) {
+           //  printf("in first meta pass linIdexMeta %d blockIdx.x %d blockDim.x %d metaData.totalMetaLength %d threadIdx.x %d \n  ", linIdexMeta, blockIdx.x, blockDim.x, metaData.totalMetaLength, threadIdx.x );
+         //}
+
+         //goldpass
+        addToQueue(linIdexMeta, 0
+            , fpFnLocCounter, localWorkQueue, localOffsetQueue, localWorkQueueCounter
+            , 1, 9, 6
+            , metaDataArr, metaData, minMaxes, workQueue);
+        //segmPass  
+        addToQueue(linIdexMeta, 1
+            , fpFnLocCounter, localWorkQueue, localOffsetQueue, localWorkQueueCounter
+            , 2, 7, 5
+            , metaDataArr, metaData, minMaxes, workQueue);
 
 
 
+        /*       addToQueue(fbArgs, old, count, tensorslice, xMeta, yMeta, zMeta, fbArgs.metaData.fpOffset, fbArgs.metaData.fpCount, 0, fbArgs.metaData.isActiveSegm, fpFnLocCounter, localWorkAndOffsetQueue, localWorkQueueCounter);
+               addToQueue(fbArgs, old, count, tensorslice, xMeta, yMeta, zMeta, fbArgs.metaData.fnOffset, fbArgs.metaData.fnCount, 1, fbArgs.metaData.isActiveGold, fpFnLocCounter, localWorkAndOffsetQueue, localWorkQueueCounter);*/
+    }
+    sync(cta);
+    if ((threadIdx.x == 0)) {
+        globalOffsetForBlock[0] = atomicAdd(&(minMaxes[12]), (fpFnLocCounter[0]));
+
+        /* if (fpFnLocCounter[0]>0) {
+             printf("\n in meta first pass global offset %d  locCounter %d \n  ", globalOffsetForBlock[0], fpFnLocCounter[0]);
+         }*/
+    };
+    if ((threadIdx.x == 1)) {
+        if (localWorkQueueCounter[0] > 0) {
+            globalWorkQueueCounter[0] = atomicAdd(&(minMaxes[9]), (localWorkQueueCounter[0]));
+        }
+    }
+    sync(cta);
+
+    //exporting to global work queue
+    //cooperative_groups::memcpy_async(cta, (&workQueue[globalWorkQueueCounter[0]]), (localWorkQueue), (sizeof(uint32_t) * localWorkQueueCounter[0]));
+
+
+    //setting offsets
+    for (uint32_t i = threadIdx.x; i < localWorkQueueCounter[0]; i += blockDim.x) {
+        workQueue[globalWorkQueueCounter[0] + i] = localWorkQueue[i];
+
+        /*        printf("FFIrst meta pass lin meta to Work Q %d is gold %d to spot %d  \n "
+            , localWorkQueue[i] - isGoldOffset*(localWorkQueue[i] >= isGoldOffset)
+                , (localWorkQueue[i] >= isGoldOffset), globalWorkQueueCounter[0] + i);*/
+
+                //FP pass
+        if (localWorkQueue[i] >= isGoldOffset) {
+            metaDataArr[(localWorkQueue[i] - isGoldOffset) * metaData.metaDataSectionLength + 5] = localOffsetQueue[i] + globalOffsetForBlock[0];
+            //printf("fp offset lin meta %d total offset  %d  global part %d local part %d \n "
+            //    , localWorkQueue[i] - isGoldOffset
+            //    , localOffsetQueue[i] + globalOffsetForBlock[0] 
+            //, globalOffsetForBlock[0]
+            //, localOffsetQueue[i]);
+
+        }
+        //FN pass
+        else {
+            metaDataArr[(localWorkQueue[i]) * metaData.metaDataSectionLength + 6] = localOffsetQueue[i] + globalOffsetForBlock[0];
+            //printf("fn offset lin meta %d total offset  %d  global part %d local part %d \n "
+            //    , localWorkQueue[i] 
+            //    , localOffsetQueue[i] + globalOffsetForBlock[0]
+            //    , globalOffsetForBlock[0]
+            //    , localOffsetQueue[i]);
+
+        };
+
+        //sync(cta);
+
+
+    }
+
+
+
+};
+
+
+
+
+
+
+
+// helper functions and utilities to work with CUDA from https://github.com/NVIDIA/cuda-samples
+
+
+
+/*
+iteration over metadata - becouse metadata may be small and to maximize occupancy we use linear index and then clalculate xMeta,ymeta,zMeta from this linear index ...
+*/
+#pragma once
+template <typename TYO>
+__global__ void getMinMaxes(ForBoolKernelArgs<TYO> fbArgs
+    , unsigned int* minMaxes
+    , TYO* goldArr, TYO* segmArr, MetaDataGPU metaData
+) {
+
+    // __global__ void getMinMaxes(unsigned int* minMaxes) {
+     ////////////some initializations
+    thread_block cta = this_thread_block();
+    //thread_block_tile<32> tile = tiled_partition<32>(cta);
+
+
+
+    //shared memory
+
+    __shared__ bool anyInGold[1];
+    //__shared__ uint32_t reduction_s[32];
+    //1)maxX 2)minX 3)maxY 4) minY 5) maxZ 6) minZ
+    __shared__ unsigned int minMaxesInShmem[7];
+
+    if ((threadIdx.x == 1) && (threadIdx.y == 0)) { minMaxesInShmem[1] = 0; };
+    if ((threadIdx.x == 2) && (threadIdx.y == 0)) { minMaxesInShmem[2] = 1000; };
+
+    if ((threadIdx.x == 3) && (threadIdx.y == 0)) { minMaxesInShmem[3] = 0; };
+    if ((threadIdx.x == 4) && (threadIdx.y == 0)) { minMaxesInShmem[4] = 1000; };
+
+    if ((threadIdx.x == 5) && (threadIdx.y == 0)) { minMaxesInShmem[5] = 0; };
+    if ((threadIdx.x == 6) && (threadIdx.y == 0)) { minMaxesInShmem[6] = 1000; };
+
+    if ((threadIdx.x == 7) && (threadIdx.y == 0)) { anyInGold[1] = false; };
+
+
+    //if ((threadIdx.x == 1) && (threadIdx.y == 0)) {
+    //    //printf("in minMaxes beg  totalMetaLength  %d Nx %d Ny %d Nz %d \n"
+    //    //    , fbArgs.metaData.totalMetaLength
+    //    //    , fbArgs.goldArr.Nx
+    //    //    , fbArgs.goldArr.Ny
+    //    //    , fbArgs.goldArr.Nz
+    //    //
+    //    //);
+
+    //    if (blockIdx.x == 0) {
+    //        printf(" dims meta in min maxes  kernel Meta X %d MetaY %d metaZ %d dbXSize %d dbYsize %d dbZsize %d minX %d minY %d minZ \n "
+    //            , metaData.metaXLength, metaData.MetaYLength, metaData.MetaZLength
+    //            , fbArgs.dbXLength, fbArgs.dbYLength, fbArgs.dbZLength
+    //            , metaData.minX, metaData.minY, metaData.minZ
+    //        );
+
+    //}
+
+    __syncthreads();
+
+    /////////////////////////
+
+
+    //main metadata iteration
+    for (auto linIdexMeta = blockIdx.x; linIdexMeta < metaData.totalMetaLength; linIdexMeta += gridDim.x) {
+        //we get from linear index  the coordinates of the metadata block of intrest
+        int  xMeta = linIdexMeta % metaData.metaXLength;
+        int   zMeta = int(floor((float)(linIdexMeta / (fbArgs.metaData.metaXLength * metaData.MetaYLength))));
+        int   yMeta = int(floor((float)((linIdexMeta - ((zMeta * metaData.metaXLength * metaData.MetaYLength) + xMeta)) / metaData.metaXLength)));
+        //iterating over data block
+        for (uint8_t xLoc = threadIdx.x; xLoc < 32; xLoc += blockDim.x) {
+            uint32_t x = xMeta * fbArgs.dbXLength + xLoc;//absolute position
+            for (uint8_t yLoc = threadIdx.y; yLoc < fbArgs.dbYLength; yLoc += blockDim.y) {
+                uint32_t  y = yMeta * fbArgs.dbYLength + yLoc;//absolute position
+                //if (y == 0) {
+                //    printf("x %d  in min maxes \n ", x);
+
+                //}
+                if (y < fbArgs.goldArr.Ny && x < fbArgs.goldArr.Nx) {
+
+                    // resetting 
+
+
+                    for (uint8_t zLoc = 0; zLoc < fbArgs.dbZLength; zLoc++) {
+                        uint32_t z = zMeta * fbArgs.dbZLength + zLoc;//absolute position
+                        if (z < fbArgs.goldArr.Nz) {
+                            //first array gold
+                            //uint8_t& zLocRef = zLoc; uint8_t& yLocRef = yLoc; uint8_t& xLocRef = xLoc;
+
+                            // setting bits
+                            bool goldBool = goldArr[x + y * fbArgs.goldArr.Nx + z * fbArgs.goldArr.Nx * fbArgs.goldArr.Ny] == fbArgs.numberToLookFor;  // (getTensorRow<TYU>(tensorslice, fbArgs.goldArr, fbArgs.goldArr.Ny, y, z)[x] == fbArgs.numberToLookFor);
+                            bool segmBool = segmArr[x + y * fbArgs.goldArr.Nx + z * fbArgs.goldArr.Nx * fbArgs.goldArr.Ny] == fbArgs.numberToLookFor;
+                            if (goldBool || segmBool) {
+                                anyInGold[0] = true;
+                                //printf(" \n in min maxes dims meta in min maxes   x %d y%d z%d xMeta %d yMeta %d zMeta %d  kernel Meta X %d MetaY %d metaZ %d dbXSize %d dbYsize %d dbZsize %d minX %d minY %d minZ %d linIdexMeta %d counted %d  \n "
+                                //    ,x,y,z,
+                                //    xMeta,yMeta,zMeta
+                                //    , metaData.metaXLength, metaData.MetaYLength, metaData.MetaZLength
+                                //    , fbArgs.dbXLength, fbArgs.dbYLength, fbArgs.dbZLength
+                                //    , metaData.minX, metaData.minY, metaData.minZ
+                                //    , linIdexMeta
+                                //    , int(floor((float)((linIdexMeta - ((zMeta * metaData.metaXLength * metaData.MetaYLength) + xMeta)) / metaData.metaXLength)))
+                                //);
+
+                            }
+
+
+
+                        }
+
+                    }
+                }
+
+                //  __syncthreads();
+                  //waiting so shared memory will be loaded evrywhere
+                  //on single thread we do last sum reduction
+
+                  /////////////////// setting min and maxes
+  //    //1)maxX 2)minX 3)maxY 4) minY 5) maxZ 6) minZ
+                auto active = coalesced_threads();
+                sync(cta);
+                active.sync();
+
+                if ((threadIdx.x == 0) && (threadIdx.y == 0) && anyInGold[0]) { minMaxesInShmem[1] = max(xMeta, minMaxesInShmem[1]); };
+                if ((threadIdx.x == 1) && (threadIdx.y == 0) && anyInGold[0]) { minMaxesInShmem[2] = min(xMeta, minMaxesInShmem[2]); };
+
+                if ((threadIdx.x == 2) && (threadIdx.y == 0) && anyInGold[0]) {
+
+                    minMaxesInShmem[3] = max(yMeta, minMaxesInShmem[3]);
+
+                    //if (minMaxesInShmem[3] > 0) {
+                    //    printf(" prim minMaxesInShmem maxY %d meta %d \n ", minMaxesInShmem[3], yMeta);
+                    //}
+
+                };
+                if ((threadIdx.x == 3) && (threadIdx.y == 0) && anyInGold[0]) { minMaxesInShmem[4] = min(yMeta, minMaxesInShmem[4]); };
+
+                if ((threadIdx.x == 4) && (threadIdx.y == 0) && anyInGold[0]) { minMaxesInShmem[5] = max(zMeta, minMaxesInShmem[5]); };
+                if ((threadIdx.x == 5) && (threadIdx.y == 0) && anyInGold[0]) {
+                    minMaxesInShmem[6] = min(zMeta, minMaxesInShmem[6]);
+                    // printf("local fifth %d  \n", minMaxesInShmem[6]);
+                };
+                // active.sync();
+                sync(cta); // just to reduce the warp divergence
+                anyInGold[0] = false;
+
+
+
+
+            }
+        }
+
+    }
+    sync(cta);
+
+    auto active = coalesced_threads();
+
+    if ((threadIdx.x == 1) && (threadIdx.y == 0)) {
+        //  printf("\n in minMaxes internal  %d \n", minMaxesInShmem[1]);
+       //getTensorRow<unsigned int>(tensorslice, fbArgs.metaData.minMaxes, fbArgs.metaData.minMaxes.Ny, 0, 0)[0] = 61;
+        atomicMax(&minMaxes[1], minMaxesInShmem[1]);
+        //atomicMax(&minMaxes[1], 2);
+       // minMaxes[1] = 0;
+    };
+
+    if ((threadIdx.x == 0) && (threadIdx.y == 0)) {
+
+        atomicMin(&minMaxes[2], minMaxesInShmem[2]);
+    };
+
+    if ((threadIdx.x == 1) && (threadIdx.y == 0)) {
+        atomicMax(&minMaxes[3], minMaxesInShmem[3]);
+        //  printf(" minMaxesInShmem maxY %d \n ", minMaxes[3]);
+
+    };
+
+    if ((threadIdx.x == 2) && (threadIdx.y == 0)) {
+        atomicMin(&minMaxes[4], minMaxesInShmem[4]);
+        //   printf(" minMaxesInShmem minY %d \n ", minMaxes[4]);
+
+    };
+
+
+
+    if (threadIdx.x == 3 && threadIdx.y == 0) {
+        atomicMax(&minMaxes[5], minMaxesInShmem[5]);
+        //  printf(" minMaxesInShmem  %d \n ", minMaxes[5]);
+    };
+
+    if (threadIdx.x == 4 && threadIdx.y == 0) {
+        atomicMin(&minMaxes[6], minMaxesInShmem[6]);
+        // printf(" minMaxesInShmem  %d \n ", minMaxes[6]);
+
+    };
+
+
+
+
+
+}
 
 
 
