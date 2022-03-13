8a65acfc44c6d1940eb5f3c81d39092b7048a52a
Jakub Mitura <53857487+jakubMitura14@users.noreply.github.com>
2022-03-13 10:17:04 +0100

    still works

--- lltm_cuda.cpp
+++ lltm_cuda.cpp
@@ -11,17 +11,6 @@ std::vector<torch::Tensor> lltm_cuda_forward(
     torch::Tensor old_h,
     torch::Tensor old_cell);
 
-std::vector<torch::Tensor> lltm_cuda_backward(
-    torch::Tensor grad_h,
-    torch::Tensor grad_cell,
-    torch::Tensor new_cell,
-    torch::Tensor input_gate,
-    torch::Tensor output_gate,
-    torch::Tensor candidate_cell,
-    torch::Tensor X,
-    torch::Tensor gate_weights,
-    torch::Tensor weights);
-
 // C++ interface
 
 // NOTE: AT_ASSERT has become AT_CHECK on master after 0.4.
@@ -44,38 +33,7 @@ std::vector<torch::Tensor> lltm_forward(
     return lltm_cuda_forward(input, weights, bias, old_h, old_cell);
 }
 
-std::vector<torch::Tensor> lltm_backward(
-    torch::Tensor grad_h,
-    torch::Tensor grad_cell,
-    torch::Tensor new_cell,
-    torch::Tensor input_gate,
-    torch::Tensor output_gate,
-    torch::Tensor candidate_cell,
-    torch::Tensor X,
-    torch::Tensor gate_weights,
-    torch::Tensor weights) {
-    CHECK_INPUT(grad_h);
-    CHECK_INPUT(grad_cell);
-    CHECK_INPUT(input_gate);
-    CHECK_INPUT(output_gate);
-    CHECK_INPUT(candidate_cell);
-    CHECK_INPUT(X);
-    CHECK_INPUT(gate_weights);
-    CHECK_INPUT(weights);
-
-    return lltm_cuda_backward(
-        grad_h,
-        grad_cell,
-        new_cell,
-        input_gate,
-        output_gate,
-        candidate_cell,
-        X,
-        gate_weights,
-        weights);
-}
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("forward", &lltm_forward, "LLTM forward (CUDA)");
-    m.def("backward", &lltm_backward, "LLTM backward (CUDA)");
 }
