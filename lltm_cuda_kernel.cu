f3dd1401e0860102235113c47ff789e31f41013b
Jakub Mitura <53857487+jakubMitura14@users.noreply.github.com>
2022-03-13 10:49:59 +0100

    still works

--- lltm_cuda_kernel.cu
+++ lltm_cuda_kernel.cu
@@ -58,40 +58,6 @@ namespace {
         }
     }
 
-    template <typename scalar_t>
-    __global__ void lltm_cuda_backward_kernel(
-        torch::PackedTensorAccessor<scalar_t, 2, torch::RestrictPtrTraits, size_t> d_old_cell,
-        torch::PackedTensorAccessor<scalar_t, 3, torch::RestrictPtrTraits, size_t> d_gates,
-        const torch::PackedTensorAccessor<scalar_t, 2, torch::RestrictPtrTraits, size_t> grad_h,
-        const torch::PackedTensorAccessor<scalar_t, 2, torch::RestrictPtrTraits, size_t> grad_cell,
-        const torch::PackedTensorAccessor<scalar_t, 2, torch::RestrictPtrTraits, size_t> new_cell,
-        const torch::PackedTensorAccessor<scalar_t, 2, torch::RestrictPtrTraits, size_t> input_gate,
-        const torch::PackedTensorAccessor<scalar_t, 2, torch::RestrictPtrTraits, size_t> output_gate,
-        const torch::PackedTensorAccessor<scalar_t, 2, torch::RestrictPtrTraits, size_t> candidate_cell,
-        const torch::PackedTensorAccessor<scalar_t, 3, torch::RestrictPtrTraits, size_t> gate_weights) {
-        //batch index
-        const int n = blockIdx.y;
-        // column index
-        const int c = blockIdx.x * blockDim.x + threadIdx.x;
-        if (c < d_gates.size(2)) {
-            const auto d_output_gate = tanh(new_cell[n][c]) * grad_h[n][c];
-            const auto d_tanh_new_cell = output_gate[n][c] * grad_h[n][c];
-            const auto d_new_cell =
-                d_tanh(new_cell[n][c]) * d_tanh_new_cell + grad_cell[n][c];
-
-
-            d_old_cell[n][c] = d_new_cell;
-            const auto d_candidate_cell = input_gate[n][c] * d_new_cell;
-            const auto d_input_gate = candidate_cell[n][c] * d_new_cell;
-
-            d_gates[n][0][c] =
-                d_input_gate * d_sigmoid(gate_weights[n][0][c]);
-            d_gates[n][1][c] =
-                d_output_gate * d_sigmoid(gate_weights[n][1][c]);
-            d_gates[n][2][c] =
-                d_candidate_cell * d_elu(gate_weights[n][2][c]);
-        }
-    }
 } // namespace
 
 std::vector<torch::Tensor> lltm_cuda_forward(
